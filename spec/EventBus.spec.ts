795a45fa4c4df5308fdc6a3f1bd449270928fd04
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-11-19 12:05:28 +0100

    Add tests for nested fragments

--- spec/EventBus.spec.ts
+++ spec/EventBus.spec.ts
@@ -3,8 +3,12 @@ import { EventBus } from "../src/EventBus"
 
 describe("EventBus", () => {
   const bus = new EventBus()
-  const mocks = { callback: vi.fn() }
+  const mocks = {
+    callback: vi.fn(),
+    otherCallback: vi.fn()
+  }
   const spy = vi.spyOn(mocks, "callback")
+  const otherSpy = vi.spyOn(mocks, "otherCallback")
 
   beforeEach(() => {
     vi.clearAllMocks()
@@ -31,6 +35,30 @@ describe("EventBus", () => {
       bus.unsubscribe("foo", mocks.callback)
       bus.emit("foo")
       expect(spy).not.toHaveBeenCalled()
+    })
+  })
+
+  describe("triggering a nested fragment", () => {
+    bus.subscribe("top/leaf", mocks.callback)
+    bus.subscribe("top", mocks.otherCallback)
+
+    it("Triggers the fragment", () => {
+      bus.emit("top/leaf")
+      expect(mocks.callback).toHaveBeenCalledOnce()
+    })
+    it("Triggers the top fragment", () => {
+      bus.emit("top/leaf")
+      expect(mocks.otherCallback).toHaveBeenCalledOnce()
+    })
+  })
+
+  describe("triggering two different callbacks", () => {
+    it("Triggers both fragments with the correct payload", () => {
+      bus.subscribe("test2", mocks.callback)
+      bus.subscribe("test2", mocks.otherCallback)
+      bus.emit("test2", { bar: "baz" })
+      expect(spy).toHaveBeenCalledExactlyOnceWith({ bar: "baz" })
+      expect(otherSpy).toHaveBeenCalledExactlyOnceWith({ bar: "baz" })
     })
   })
 
